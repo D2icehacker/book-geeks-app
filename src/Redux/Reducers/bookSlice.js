a38077c8af04858844f349a40421d8484daa0aa1
ph-dev-labs <85322356+ph-dev-labs@users.noreply.github.com>
2024-05-15 06:16:05 +0100

    redux setup completed

---
+++ src/Redux/Reducers/bookSlice.js
@@ -0,0 +1,33 @@
+// src/features/books/bookSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  bookmarked: [],
+  favorites: [],
+};
+
+export const bookSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBookmark: (state, action) => {
+      state.bookmarked.push(action.payload);
+    },
+    removeBookmark: (state, action) => {
+      state.bookmarked = state.bookmarked.filter(book => book.id !== action.payload.id);
+    },
+    addFavorite: (state, action) => {
+      state.favorites.push(action.payload);
+    },
+    removeFavorite: (state, action) => {
+      state.favorites = state.favorites.filter(book => book.id !== action.payload.id);
+    },
+  },
+});
+
+export const { addBookmark, removeBookmark, addFavorite, removeFavorite } = bookSlice.actions;
+
+export const selectBookmarked = (state) => state.books.bookmarked;
+export const selectFavorites = (state) => state.books.favorites;
+
+export default bookSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import store from './Redux/store';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
